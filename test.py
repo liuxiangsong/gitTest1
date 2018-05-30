67d4a0e72e99e5769eafcfa8bf4c34ce09eb0f87
Your Name <pacid@foxmail.com>
2018-05-30 16:25:28 +0800

    添加test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+import urllib.request
+from bs4 import BeautifulSoup
+response = urllib.request.urlopen('https://www.cnblogs.com/')
+html = response.read()
+soup = BeautifulSoup(html, 'lxml')
+items = soup.select(".post_item_body a[class='titlelnk']")
+for item in items:
+    print(item.string)
