0af31f57eec574a13c96e94a0fb7bc90b8358f96
pacid <pacid@foxmail.com>
2018-05-30 23:42:18 +0800

    modify readme

--- crawlDeppon.py
+++ crawlDeppon.py
@@ -5,19 +5,12 @@ from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 
-
-# response = urllib.request.urlopen('https://www.deppon.com/deptlist/')
-# # time.sleep(10)
-# html = response.read()
-# soup = BeautifulSoup(html, 'lxml')
-# print(soup)
-# items = soup.select("div[class='listA listA_Z'] a")
-# print(items)
-# for item in items:
-#     print(item.string)
-
 driver = webdriver.Chrome(chrome_options=chrome_options,
                           executable_path="./chromedriver")
 driver.get("https://www.deppon.com/deptlist/")
 html = driver.page_source
-print(html)
+soup = BeautifulSoup(html, 'lxml')
+items = soup.select('div[class~="listA_Z"] a')
+for item in items:
+    print(item.string)
+
